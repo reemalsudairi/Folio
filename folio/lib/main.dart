ba2f57a5801014750a82f86d6a4385951ba78b2e
GhalaAng <145207757+GhalaAng@users.noreply.github.com>
2024-09-08 11:53:19 +0300

    u

--- folio/lib/main.dart
+++ folio/lib/main.dart
@@ -2,8 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+    WidgetsFlutterBinding.ensureInitialized(
+    );
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyD08AQh_ozf3QSFAZcg7eTWGtBMJw_kG_0",
+      appId: "1:866432989267:android:57f897cf2489c0a5d56ea6",
+      messagingSenderId: "866432989267",
+      projectId: "folio-29339",)
+  );
   runApp(const MyApp());
 }
 
