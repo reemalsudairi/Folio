3f4c01f94a97d764e6baf795ac5433751e960680
reemalsudairi <reemalsedairi@gmail.com>
2024-09-09 18:47:45 +0300

    i

---
+++ folio/lib/screens/homebage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Homebage extends StatefulWidget{
+  @override
+  State<Homebage> createState() => _HomePageState();
+
+}
+
+class _HomePageState extends State<Homebage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+
+}
