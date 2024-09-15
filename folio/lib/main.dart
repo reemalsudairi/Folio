53f5ec2356dfd350bfd27acd5a10c9fa99b13f8e
haifams <158820968+haifams@users.noreply.github.com>
2024-09-15 21:52:23 +0300

    k

--- folio/lib/main.dart
+++ folio/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:folio/screens/LoginPage.dart';
 import 'package:folio/screens/homePage.dart';
 
 void main() async {
@@ -42,7 +43,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoginPage(),
     );
   }
 }
