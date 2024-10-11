c1f18ab86a804890de86bfbe215bd41d4cd1f59a
mashael-qq1 <meshael.qq1@gmail.com>
2024-10-11 07:16:41 +0300

    bookclub page

---
+++ folio/lib/screens/bookclubs_page.dart
@@ -0,0 +1,191 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Club {
+  final String id;
+  final String name;
+  final String picture;
+
+  Club({required this.id, required this.name, required this.picture});
+
+  factory Club.fromMap(Map<String, dynamic> data, String documentId) {
+    return Club(
+      id: documentId,
+      name: data['name'] ?? '',
+      picture: data['picture'] ?? '',
+    );
+  }
+}
+
+class Clubs extends StatelessWidget {
+  const Clubs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Book Clubs',style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 26,
+            color: Color(0xFF351F1F),
+          ),),
+        backgroundColor: Color(0xFFF8F8F3),
+      ),
+      backgroundColor: Color(0xFFF8F8F3), // Set background color here
+      body: const ClubsBody(),
+    );
+  }
+}
+
+class ClubsBody extends StatefulWidget {
+  const ClubsBody({super.key});
+
+  @override
+  _ClubsBodyState createState() => _ClubsBodyState();
+}
+
+class _ClubsBodyState extends State<ClubsBody> {
+  List<Club> clubs = [];
+  List<Club> filteredClubs = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchClubs();
+  }
+
+  Future<void> fetchClubs() async {
+    try {
+      QuerySnapshot clubsSnapshot =
+          await FirebaseFirestore.instance.collection('clubs').get();
+
+      setState(() {
+        clubs = clubsSnapshot.docs
+            .map((doc) => Club.fromMap(doc.data() as Map<String, dynamic>, doc.id))
+            .toList();
+        filteredClubs = clubs; // Initially, show all clubs
+      });
+    } catch (e) {
+      print('Error fetching clubs: $e');
+    }
+  }
+
+  void filterClubs(String query) {
+    List<Club> searchResults = [];
+    if (query.isNotEmpty) {
+      searchResults = clubs
+          .where((club) =>
+              club.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    } else {
+      searchResults = clubs;
+    }
+    setState(() {
+      filteredClubs = searchResults;
+    });
+  }
+
+  Widget buildClubCard(Club club) {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          club.picture.isNotEmpty
+              ? ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: SizedBox(
+                    height: 140,
+                    width: double.infinity,
+                    child: Image.network(
+                      club.picture,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Container(
+                          height: 140,
+                          color: Colors.red,
+                          child: Center(
+                              child: Text('No Image Available',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 16))),
+                        );
+                      },
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress == null) return child;
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      },
+                    ),
+                  ),
+                )
+              : SizedBox(
+                  height: 140,
+                  width: double.infinity,
+                  child: Center(
+                      child: Text('No Image Available',
+                          style: TextStyle(fontSize: 16))),
+                ),
+          SizedBox(height: 10),
+          Text(
+            club.name,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Search bar
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            onChanged: (value) {
+              filterClubs(value);
+            },
+            decoration: InputDecoration(
+              hintText: 'Search for a club',
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide.none,
+              ),
+              fillColor: Colors.grey[200],
+              filled: true,
+            ),
+          ),
+        ),
+        // List of clubs
+        Expanded(
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.75,
+            ),
+            itemCount: filteredClubs.length,
+            itemBuilder: (context, index) {
+              return buildClubCard(filteredClubs[index]);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
