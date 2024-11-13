3c1689651dfdda653d6cbb4782d4edc96cab3ce1
GhalaAng <145207757+GhalaAng@users.noreply.github.com>
2024-11-13 20:02:51 +0300

    admin login

--- folio/lib/admin_screens/AdminLoginPage.dart
+++ folio/lib/admin_screens/AdminLoginPage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -20,34 +21,49 @@ class _AdminLoginPageState extends State<AdminLoginPage> {
   bool _obscurePassword = true;
 
   Future<void> signAdminIn() async {
-    if (_formKey.currentState?.validate() ?? false) {
-      FocusScope.of(context).unfocus();
+  if (_formKey.currentState?.validate() ?? false) {
+    FocusScope.of(context).unfocus();
 
-      try {
-        UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: emailController.text.trim(),
-          password: passwordController.text.trim(),
-        );
+    try {
+      // Convert email to lowercase for case-insensitive comparison
+      String email = emailController.text.trim().toLowerCase();
+      String password = passwordController.text.trim();
 
-        // Navigate to the admin dashboard after successful login
+      // Log the email and password being used for the login attempt
+      print("Attempting to sign in with email: $email and password: $password");
+
+      // Fetch the admin document from Firestore
+      final QuerySnapshot result = await FirebaseFirestore.instance
+          .collection('admin')
+          .where('email', isEqualTo: email)
+          .where('password', isEqualTo: password)
+          .get();
+
+      // Check if any admin document was found
+      if (result.docs.isNotEmpty) {
+        String adminUsername = result.docs.first['username'];
+        // Admin found, navigate to the dashboard
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => AdminDashboard(),
+            builder: (context) => AdminDashboard(adminUsername: adminUsername),
           ),
         );
-      } on FirebaseAuthException catch (e) {
-        // Handle authentication errors
+      } else {
+        // No admin found with the provided credentials
         setState(() {
-          _errorMessage = _handleAuthError(e); // Set the error message
-        });
-      } catch (e) {
-        setState(() {
-          _errorMessage = "An unexpected error occurred."; // Handle unexpected errors
+          _errorMessage = 'Invalid email or password. Please try again.';
         });
       }
+    } catch (e) {
+      print("Unexpected error: $e"); // Log unexpected errors
+      setState(() {
+        _errorMessage = "An unexpected error occurred."; // Handle unexpected errors
+      });
     }
   }
+}
+
 
   String _handleAuthError(FirebaseAuthException error) {
     switch (error.code) {
@@ -125,141 +141,141 @@ class _AdminLoginPageState extends State<AdminLoginPage> {
   }
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: const Color(0xFFF8F8F3),
-    body: Center( // Centering the body content
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, // Centering the column content
-              crossAxisAlignment: CrossAxisAlignment.center, // Centering the content
-              children: [
-                Image.asset(
-                  "assets/images/Logo.png",
-                  width: 500,
-                  height: 300,
-                  fit: BoxFit.cover,
-                ),
-                const SizedBox(height: 1), // Reduced space between image and text
-                const Text(
-                  'Admin Login', // Text to display
-                  style: TextStyle(
-                    fontSize: 24, // Font size
-                    fontWeight: FontWeight.bold, // Bold text
-                    color: Color(0xFF695555), // Text color
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF8F8F3),
+      body: Center( // Centering the body content
+        child: SingleChildScrollView (
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, // Centering the column content
+                crossAxisAlignment: CrossAxisAlignment.center, // Centering the content
+                children: [
+                  Image.asset(
+                    "assets/images/Logo.png",
+                    width: 500,
+                    height: 300,
+                    fit: BoxFit.cover,
                   ),
-                ),
-                const SizedBox(height: 20), // Space between text and email field
-
-                // Display error message if exists
-                if (_errorMessage != null)
-                  Container(
-                    width: 350,
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Text(
-                      _errorMessage!,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                      ),
+                  const SizedBox(height: 1), // Reduced space between image and text
+                  const Text(
+                    'Admin Login', // Text to display
+                    style: TextStyle(
+                      fontSize: 24, // Font size
+                      fontWeight: FontWeight.bold, // Bold text
+                      color: Color(0xFF695555), // Text color
                     ),
                   ),
-                const SizedBox(height: 20),
+                  const SizedBox(height: 20), // Space between text and email field
 
-                // Email Field Title and Text Field
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, // Align title to the left
-                  children: [
-                    const Text(
-                      'Email',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF695555),
+                  // Display error message if exists
+                  if (_errorMessage != null)
+                    Container(
+                      width: 350,
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(8),
                       ),
-                    ),
-                    const SizedBox(height: 5),
-                    _buildTextField(
-                      controller: emailController,
-                      hintText: 'Email',
-                      maxLength: 254, // Set the max length for the email
-                      validator: (value) {
-                        if (value == null || value.trim().isEmpty) {
-                          return 'Please enter your email';
-                        }
-                        return null;
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 15),
-
-                // Password Field Title and Text Field
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, // Align title to the left
-                  children: [
-                    const Text(
-                      'Password',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF695555),
-                      ),
-                    ),
-                    const SizedBox(height: 5),
-                    _buildTextField(
-                      controller: passwordController,
-                      hintText: 'Password',
-                      obscureText: true,
-                      maxLength: 16, // Set the max length for the password
-                      validator: (value) {
-                        if (value == null || value.trim().isEmpty) {
-                          return 'Please enter your password';
-                        }
-                        return null;
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 40),
-
-                // Login Button
-                SizedBox(
-                  width: 350,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFFF790AD),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40),
-                      ),
-                    ),
-                    onPressed: signAdminIn,
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10), // Reduced vertical padding
                       child: Text(
-                        'Login',
-                        style: TextStyle(
+                        _errorMessage!,
+                        style: const TextStyle(
                           color: Colors.white,
-                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  const SizedBox(height: 20),
+
+                  // Email Field Title and Text Field
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, // Align title to the left
+                    children: [
+                      const Text(
+                        'Email',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF695555),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      _buildTextField(
+                        controller: emailController,
+                        hintText: 'Email',
+                        maxLength: 254, // Set the max length for the email
+                        validator: (value) {
+                          if (value == null || value.trim().isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+
+                  // Password Field Title and Text Field
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, // Align title to the left
+                    children: [
+                      const Text(
+                        'Password',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF695555),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      _buildTextField(
+                        controller: passwordController,
+                        hintText: 'Password',
+                        obscureText: true,
+                        maxLength: 16, // Set the max length for the password
+                        validator: (value) {
+                          if (value == null || value.trim().isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 40),
+
+                  // Login Button
+                  SizedBox(
+                    width: 350,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFF790AD),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                      onPressed: signAdminIn,
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10), // Reduced vertical padding
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
--- folio/lib/admin_screens/admin_dashboard.dart
+++ folio/lib/admin_screens/admin_dashboard.dart
@@ -1,147 +1,93 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import 'AdminLoginPage.dart'; // Import your login page
+import 'AdminLoginPage.dart';
 
 class AdminDashboard extends StatefulWidget {
+  final String adminUsername;
+
+  const AdminDashboard({Key? key, required this.adminUsername}) : super(key: key);
+
   @override
   _AdminDashboardState createState() => _AdminDashboardState();
 }
 
 class _AdminDashboardState extends State<AdminDashboard> {
-  String? adminUsername; // Make this nullable
-  bool isLoading = true; // Loading state
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchAdminUsername();
-  }
-
-  // Fetch the admin's username from Firestore
-  Future<void> _fetchAdminUsername() async {
-    try {
-      User? currentUser = FirebaseAuth.instance.currentUser;
-      if (currentUser != null) {
-        String email = currentUser.email!;
-        // Query Firestore to get the document where the email matches
-        QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-            .collection('admin')
-            .where('email', isEqualTo: email)
-            .limit(1)
-            .get();
-
-        if (querySnapshot.docs.isNotEmpty) {
-          var userDoc = querySnapshot.docs.first;
-          setState(() {
-            adminUsername = userDoc.get('username'); // Retrieve admin's username
-            isLoading = false; // Set loading to false after fetching
-          });
-        } else {
-          print("No document found for email $email in Firestore.");
-          setState(() {
-            isLoading = false; // Set loading to false if no document is found
-          });
-        }
-      }
-    } catch (e) {
-      print("Error fetching admin username: $e");
-      setState(() {
-        isLoading = false; // Set loading to false in case of error
-      });
-    }
-  }
-
-  // Function to show logout confirmation dialog
- // Function to show logout confirmation dialog
-void _showLogoutConfirmationDialog() {
-  showDialog(
-    context: context,
-    barrierDismissible: false, // Disable dismissal by clicking outside
-    builder: (context) => Dialog(
-      backgroundColor: Colors.transparent,
-      child: Container(
-        width: 300, // Set a specific width for the dialog
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: const Color(0xFFF790AD).withOpacity(0.9), // Pinkish background with opacity
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(
-              Icons.exit_to_app, // Icon for sign out
-              color: Colors.white,
-              size: 40,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Are you sure you want to log out?',
-              style: const TextStyle(
+  void _showLogoutConfirmationDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => Dialog(
+        backgroundColor: Colors.transparent,
+        child: Container(
+          width: 300,
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: const Color(0xFFF790AD).withOpacity(0.9),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.exit_to_app,
                 color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+                size: 40,
               ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                // Yes button
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(); // Close the dialog
-                    _logout(); // Call the logout function
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green, // Change to desired color
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
-                  child: const Text(
-                    'Yes',
-                    style: TextStyle(color: Colors.white),
-                  ),
+              const SizedBox(height: 10),
+              Text(
+                'Are you sure you want to log out?',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
                 ),
-                // Cancel button
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(); // Close the dialog
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red, // Change to desired color
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      _logout();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
                     ),
+                    child: const Text('Yes', style: TextStyle(color: Colors.white)),
                   ),
-                  child: const Text(
-                    'Cancel',
-                    style: TextStyle(color: Colors.white),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: const Text('Cancel', style: TextStyle(color: Colors.white)),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-
-
-  // Function to handle logout
   Future<void> _logout() async {
     await FirebaseAuth.instance.signOut();
-    // Navigate to the login page
     Navigator.pushAndRemoveUntil(
       context,
-      MaterialPageRoute(builder: (context) => AdminLoginPage()), // Replace with your actual login page widget
-      (route) => false, // Remove all previous routes
+      MaterialPageRoute(builder: (context) => AdminLoginPage()),
+      (route) => false,
     );
   }
 
@@ -150,47 +96,41 @@ void _showLogoutConfirmationDialog() {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(0.0), // No padding for the top
+          padding: const EdgeInsets.all(0.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Row for the image, greeting, and logout button
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  // Greeting Text with margin and bold style
                   Padding(
-                    padding: const EdgeInsets.only(left: 20.0), // Add left margin
-                    child: isLoading
-                        ? CircularProgressIndicator()
-                        : Text(
-                            'Hello, Admin ${adminUsername ?? "Admin"}',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold, // Make text bolder
-                            ),
-                          ),
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      'Hello, Admin ${widget.adminUsername}!',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                  // Cropped Image
                   Container(
-                    height: 90, // Cropped height (adjust as needed)
-                    width: 170, // Keep the width same
+                    height: 90,
+                    width: 170,
                     child: ClipRect(
                       child: Align(
                         alignment: Alignment.center,
-                        heightFactor: 0.5, // Adjust to crop more from top and bottom
+                        heightFactor: 0.5,
                         child: Image.asset(
                           'assets/images/Logo.png',
                           width: 170,
-                          fit: BoxFit.cover, // Ensures the image covers the entire container
+                          fit: BoxFit.cover,
                         ),
                       ),
                     ),
                   ),
-                  // Logout Button with right margin
                   Padding(
-                    padding: const EdgeInsets.only(right: 20.0), // Add right margin
+                    padding: const EdgeInsets.only(right: 20.0),
                     child: ElevatedButton(
                       onPressed: () => _showLogoutConfirmationDialog(),
                       style: ElevatedButton.styleFrom(
@@ -207,13 +147,12 @@ void _showLogoutConfirmationDialog() {
                   ),
                 ],
               ),
-              const SizedBox(height: 0), // Reduced space below the row
-              // Grey line under the row
+              const SizedBox(height: 0),
               Container(
-                height: 1, // Height of the line
-                color: const Color.fromARGB(255, 216, 216, 216), // Color of the line
+                height: 1,
+                color: const Color.fromARGB(255, 216, 216, 216),
               ),
-              const SizedBox(height: 10), // Adjusted space below the line
+              const SizedBox(height: 10),
             ],
           ),
         ),
